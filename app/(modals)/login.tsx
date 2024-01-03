5e3bb3bc34e2c7ca9ea3b7c5aa5a42bd44488b71
Ievgen Vorontsov <evgeniyvorontsov1@gmail.com>
2024-01-03 19:29:29 +0200

    first commit

---
+++ app/(modals)/login.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Page = () => {
+  return (
+    <View>
+      <Text>Login</Text>
+    </View>
+  );
+}
+
+export default Page;
+
